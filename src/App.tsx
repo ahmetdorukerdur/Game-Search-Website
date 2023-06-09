424b700c71d8c73a5eb91208f34604105cb8b949
Doruk Erdur <Doruk.Erdur@fups.com>
2023-06-09 17:42:08 +0300

    Upside navbar and dark,light mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 
+export const ToggleWrapper = React.createContext({ themeChange: false, setThemeChange: (themeChange: any) => {}});
 function App() {
+  const [themeChange, setThemeChange] = useState(false);
   return (
-    <div className="App">
+    <ToggleWrapper.Provider value={{ themeChange, setThemeChange}}>
+    <div className='App'>
       <NavBar />
     </div>
+    </ToggleWrapper.Provider>
   );
 }
 

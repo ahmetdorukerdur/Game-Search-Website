6ff58c6c16ef5dc1f16f8070e7bb5fdd75b7420d
Doruk Erdur <Doruk.Erdur@fups.com>
2023-06-13 18:07:14 +0300

    dark and light mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import MainScreen from './components/MainScreen';
 
 export const ToggleWrapper = React.createContext({ themeChange: false, setThemeChange: (themeChange: any) => {}});
 function App() {
   const [themeChange, setThemeChange] = useState(false);
   return (
     <ToggleWrapper.Provider value={{ themeChange, setThemeChange}}>
-    <div className='App'>
-      <NavBar />
-    </div>
+      <div className='App'>
+        <NavBar />
+        <MainScreen />
+      </div>
     </ToggleWrapper.Provider>
   );
 }
